097ee7598fad182c50204fc64e7ad78a0a41f6ff
Nguyễn Thái Son Ny <134358623+sonnynguyenthai@users.noreply.github.com>
2025-04-01 10:55:12 +1100

    feature/init-config

---
+++ app/(tabs)/save.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Save = () => {
+    return (
+        <View>
+            <Text>Save</Text>
+        </View>
+    )
+}
+
+export default Save
+
+const styles = StyleSheet.create({})
